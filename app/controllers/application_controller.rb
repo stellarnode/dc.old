5d01bd772d997e40b5aed2e9af2fbc382badebb9
Almazzzzz <badrtdinov_a@mail.ru>
2016-07-22 15:27:59 +0300

    add: auth w FB; installed 'figaro' gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,21 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception, prepend: true
+  	protect_from_forgery with: :exception, prepend: true
+	before_filter :ensure_signup_complete, only: [:new, :create, :update, :destroy]
+  
+	def ensure_signup_complete
+	  # Ensure we don't go into an infinite loop
+	  return if action_name == 'finish_signup'
+	  # Redirect to the 'finish_signup' page if the user
+	  # email hasn't been verified yet
+	  if current_user && !current_user.email_verified?
+	    redirect_to finish_signup_path(current_user)
+	  end
+	end
+
+	private
+	
+	def user_not_authorized
+	  flash[:alert] = "You are not authorized to perform this action."
+	  redirect_to(request.referrer || root_path)
+	end	
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160722100155) do
+ActiveRecord::Schema.define(version: 20160722104038) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "identities", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "provider"
+    t.string   "uid"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_identities_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
@@ -53,4 +62,5 @@ ActiveRecord::Schema.define(version: 20160722100155) do
     t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true, using: :btree
   end
 
+  add_foreign_key "identities", "users"
 end
